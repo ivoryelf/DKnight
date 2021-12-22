7cd05407c1ecbecd947b156b3889b988ea9cc4fa
ivoryelf <ivoryelf@hotmail.com>
2021-12-23 05:26:33 +0900

    attack add etc
    공격과 무기 추가

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -9,7 +9,19 @@ public class Player : MonoBehaviour
     float speed = 10f;
     float rspeed = 5f;
     Vector3 move_vec;
-    //Vector3 dir;
+    Animator anim;
+    Rigidbody rigid;
+    bool walk_down;
+    bool fire_down;
+    bool fire_ready;
+    float fire_delay;
+    Weapon equip_weapon;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +31,43 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Move();
+        Attack();
+    }
+
+    void GetInput()
+    {
         h_axis = Input.GetAxisRaw("Horizontal");
         v_axis = Input.GetAxisRaw("Vertical");
-        move_vec = new Vector3(h_axis, 0, v_axis).normalized;
-        if(!(h_axis==0 && v_axis == 0))
-        {
-            transform.position += move_vec * speed * Time.deltaTime;
-            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(move_vec), Time.deltaTime * rspeed);
+        walk_down = Input.GetButton("Walk");
+        fire_down = Input.GetButton("Fire1");
+    }
 
+    void Move()
+    {
+        move_vec = new Vector3(h_axis, 0, v_axis).normalized;
+        if (!(h_axis == 0 && v_axis == 0))
+        {
+            transform.position += move_vec * speed * (walk_down ? 0.3f : 1f) * Time.deltaTime;
+            anim.SetBool("isRun", move_vec != Vector3.zero);
+            anim.SetBool("isWalk", walk_down);
+            transform.LookAt(transform.position + move_vec);
+        }
+    }
+
+    void Attack()
+    {
+       //if (equip_weapon == null)
+       //     return;
+        fire_delay += Time.deltaTime;
+        fire_ready = equip_weapon.rate < fire_delay;
+
+        if(fire_down)
+        {
+            equip_weapon.Use();
+            anim.SetTrigger("trAttack");
+            fire_delay = 0;
         }
     }
 }
---
+++ Assets/Script/Weapon.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public enum Type { Melee, Range };
+    public Type type;
+    public int damage;
+    public float rate;
+    public BoxCollider meleeArea;
+    public TrailRenderer trail_effect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Use()
+    {
+        if(type == Type.Melee)
+        {
+            Attack();
+        }
+    }
+
+    public void Attack()
+    {
+
+    }
+}
