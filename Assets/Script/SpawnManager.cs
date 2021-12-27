5e2fdee1142f592589efc41c5eec7517b05bf246
ivoryelf <ivoryelf@hotmail.com>
2021-12-28 07:28:50 +0900

    fix bug and spawnmanger
    스폰 메니저 기본 제작

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -8,26 +8,33 @@ public class SpawnManager : MonoBehaviour
     public GameObject enemy;
     public Transform[] spawnPoints;
     public int max_sub_level;
-    public int[] level_enemys = int[10];//sub levels max : 10ea(by 1 stage ) 
+    public int[] level_enemys;
+    public int e_cnt, max_cnt; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        max_cnt = spawnPoints.Length;
+        e_cnt = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(e_cnt < max_cnt)
+        {
+            int x = Random.Range(0, spawnPoints.Length);
+            Instantiate(enemy, spawnPoints[x]);
+            e_cnt++;
+        }
     }
 
     void GenerateEnemy(int level, int num_enemy)
     {
-        level_enemys[level] = num_enemy;
-        foreach(int i in num_enemy)
+/*        level_enemys[level] = num_enemy;
+        for(int i: i < num_enemy: i++)
         {
             Instantiage(enemy, spawnPoints[i]);
-        }
+        }*/
     }
 
 }
